49cbb56f4bc4956579fdc86021d9e7651441c634
VMrGamer <v.mr.gamer@gmail.com>
2019-02-15 02:02:24 +0530

    guard alert success

--- triggers/functions/index.js
+++ triggers/functions/index.js
@@ -5,16 +5,46 @@ exports.newVehicle = functions.firestore
     .document('log-vehicle/{timestamp}')
     .onCreate((snap, context) => {
       const newValue = snap.data();
-
+      var db = admin.firestore();
+      var usersRef = db.collection('users');
       // This will be the master function for log-vehicle document
 
       //Check for resident
       if(newValue.resident == false){
-        
+        var query = usersRef.where('user_type', '==', 'guard').get()
+          .then(snapshot => {
+            if(snapshot.empty){
+              console.log('No Matching Documents.');
+              return 'Somethin Fishy';
+            }
+
+            snapshot.forEach(doc => {
+              var message = {
+                notification: {
+                  title: 'New Non Resient Vehicle Entered',
+                  body: 'Vehicle No: '.concat(newValue.vehicle_no)
+                },
+                token: doc.data().token
+              };
+              admin.messaging().send(message)
+                .then((response) => {
+                  console.log('Successfully sent message:', response);
+                })
+                .catch((error) => {
+                  console.log('Error sending message:', error);
+                });
+            });
+            
+          }).catch(err =>{
+            console.log('Error getting Documents', err);
+          });
       }
 
       //TODO: Get registrationID token here
       //TODO: Figure out a way to map Resident: true to another function to update the resident history data
-      //TODO: Figure out a way to map Resident: false data to another function, to alert the guard of new vehicle entered
-      
+      return 'complete';
+    });
+
+exports.test = functions.https.onRequest((req, res) => {
+        res.status(200).send(ret,'yoooooo');
     });
