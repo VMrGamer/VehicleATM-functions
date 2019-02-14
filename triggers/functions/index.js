4ebcca3ab4c8bb9e9ddbd23878457706f64db52d
VMrGamer <v.mr.gamer@gmail.com>
2019-02-14 23:35:35 +0530

    cleanup and initialize

--- triggers/functions/index.js
+++ triggers/functions/index.js
@@ -5,20 +5,16 @@ exports.newVehicle = functions.firestore
     .document('log-vehicle/{timestamp}')
     .onCreate((snap, context) => {
       const newValue = snap.data();
-      const name = newValue.name;
-        var registrationToken = 'cIShh1x74U4:APA91bEfoc0gwPnnKJyaewL0EWwykUkup0JJbMEZ2QSQyUmgDv8iYmkKBiJJG1d65uzqt_6E6EnENbcoz0u5gpoaP9HiWnDBp5SQN8reQhy6ICr9e0eZ-4lvV3eq0DJ-gx1MzFHQzUjw';
-        var message = {
-            notification: {
-                title: newValue.vehicle_no,
-                body: newValue.vehicle_no
-            },  
-            token: registrationToken
-        };
-        admin.messaging().send(message)
-            .then((response) => {
-                console.log('Successfully sent message:', response);
-            })
-            .catch((error) => {
-                console.log('Error sending message:', error);
-            });
+
+      // This will be the master function for log-vehicle document
+
+      //Check for resident
+      if(newValue.resident == false){
+        
+      }
+
+      //TODO: Get registrationID token here
+      //TODO: Figure out a way to map Resident: true to another function to update the resident history data
+      //TODO: Figure out a way to map Resident: false data to another function, to alert the guard of new vehicle entered
+      
     });
